d405c36ed913955c922c2ccbfa6e689f24354b93
romi1996 <romi1996v@gmail.com>
2018-03-18 01:51:03 +0700

    HTTP/1.1 404 Not Found
    Changing Para

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-dpkg-scanpackages -m . /dev/null >Packages
+dpkg-scanpackages -m debs . >Packages
 bzip2 -c9k ./Packages > ./Packages.bz2
 gzip -c9k ./Packages > ./Packages.gz
 printf "Origin: tor's Repo\nLabel: tor\nSuite: stable\nVersion: 1.0\nCodename: tor\nArchitecture: iphoneos-arm\nComponents: main\nDescription: tor's Tweaks\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
