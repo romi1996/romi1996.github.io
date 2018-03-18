60f02fd037256702de2dafe0d2ed52ec4c149cad
romi1996 <romi1996v@gmail.com>
2018-03-19 03:48:08 +0700

    Update string compatible

--- DEBIAN PACKAGER/repack.sh
+++ DEBIAN PACKAGER/repack.sh
@@ -23,22 +23,57 @@ set -x
 #Processing-request-PHASE2
 cd $debfol
 cd DEBIAN
-if grep -q Maintainer /control
+if grep -q Maintainer control
 	then
 		echo "Path string detected Maintainer"
-		echo -e "$(grep Maintainer /control)\n"
-	else
-		echo "Missing Maintainer"
-		echo "Added stock Maintainer to Deb Controller"
-		sed -i 's/^ *//; s/ *$//; /^$/d' control
-		read -p "Please add Maintainer name =>>>>>>" maint
-		echo "Maintainer: $maint" >> control
-		echo "Building deb ..."
+        else
+                echo "Missing Maintainer 0x1"
+                echo "You need to add Maintainer to Deb Controll"
+                echo "Added stock Maintainer to Deb Controller"
+                sed -i 's/^ *//; s/ *$//; /^$/d' control
+                read -p "Please add Maintainer name =>>>>>>" maint
+                echo "Maintainer: $maint" >> control
 fi
+if grep -q Package control
+	then
+		echo "Path string detected Package"
+		read -p "You may want to change this deb name =>>>>>" debn
+		cat control | grep -o "Package.*"
+		sed -i -e "/Package/c\Package: $debn" control
+	else
+		echo "Terminal Error : 0x0 Deb Broken"
+		exit 0
+fi
+if grep -q Depiction control
+	then
+		echo "Path string detected Depiction"
+		read -p "You may want to change this deb Depiction =>>>>>" debd
+		cat control | grep -o "Depiction.*"
+		sed -i -e "/Depiction/c\Depiction: $debd" control
+	else
+		echo "Terminal Error : 0x0 Deb Broken"
+		exit 0
+fi
+if grep -q Section control
+	then
+		echo "Path string detected Section"
+		read -p "You may want to set category for this deb =>>>>>" debsec
+		cat control | grep -o "Section.*"
+		sed -i -e "/Section/c\Section: $debsec" control
+	else
+		echo "Terminal Error : 0x0 Deb Broken"
+		exit 0
+fi
+#Processing-request-PHASE2
+
+#Processing-request-PHASE3
 cd ..
 cd ..
 dpkg-deb -b $debfol
+#Processing-request-PHASE3
+
+#FINAL
 read -p "DONE ... Press any key to end!"
 
 set +x
-#Processing-request
+#FINAL
